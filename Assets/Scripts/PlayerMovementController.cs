ed72c166fbfe2f67ae135b74904c102f0d90a71a
MrArmand <dellavmaster@gmail.com>
2023-03-01 18:50:14 +0000

    working base

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -7,40 +7,51 @@ using UnityEngine;
 public class PlayerMovementController : MonoBehaviour
 {
     [SerializeField] private float rotationSpeed;
-    [SerializeField] private float ySpeed;
+    [SerializeField] private float thrust;
+    [SerializeField] private float groundCheckDistance;
+    public LayerMask groundLayer;
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private bool isGrounded;
 
-    private Rigidbody2D rb2D;
-    private Vector2 velocity = new Vector2();
-
-    private void Awake()
+    private void Start()
     {
-        rb2D = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
-
-        velocity = rb2D.velocity;
-
         if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(Vector3.back, rotationSpeed * Time.deltaTime);
         }
-
-        if (Input.GetKey(KeyCode.A)) 
+        else if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
         }
 
+        // Check for ground contact
+        float spriteHeight = spriteRenderer.bounds.size.y;
+        Vector2 checkPos = transform.position - Vector3.up * (spriteHeight / 2);
+        RaycastHit2D hit = Physics2D.Raycast(checkPos, -Vector2.up, groundCheckDistance, groundLayer);
+        isGrounded = hit.collider != null;
+
+        // Freeze game if player touches ground
+        if (isGrounded)
+        {
+            Time.timeScale = 0f;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Apply thrust
         if (Input.GetKey(KeyCode.W))
         {
-            velocity.y = ySpeed;
+            Vector2 thrustVector = transform.up;
+            rb.AddForce(thrust * thrustVector * Time.fixedDeltaTime);
         }
-
-        rb2D.velocity = velocity;
-
-
     }
-    
 }
 
