6b4fe941468ca8f76befcd16fee59c87440ddfea
MrArmand <dellavmaster@gmail.com>
2023-05-03 17:37:22 +0100

    Added fuel and fuel UI

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -1,23 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Rigidbody2D))]
 
 public class PlayerMovementController : MonoBehaviour
 {
+
     [SerializeField] private float rotationSpeed;
     [SerializeField] private float thrust;
     [SerializeField] private float groundCheckDistance;
+    [SerializeField] private Text fuelText;
+    [SerializeField] private int fuel = 1000;
+    [SerializeField] private float comsumption = 50;
     public LayerMask groundLayer;
     private Rigidbody2D rb;
     private SpriteRenderer spriteRenderer;
     private bool isGrounded;
-
+    
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        fuelText.text += fuel.ToString();
     }
 
     private void Update()
@@ -41,7 +47,10 @@ public class PlayerMovementController : MonoBehaviour
         {
             Vector2 thrustVector = transform.up;
             rb.AddForce(thrust * thrustVector * Time.fixedDeltaTime);
+            fuel -= (int) (comsumption * Time.fixedDeltaTime);
+            
         }
+        fuelText.text += fuel.ToString();
     }
 
     public void onGroundChange(GameObject _onGround)
