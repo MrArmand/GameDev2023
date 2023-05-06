57fa7b8e8fe5a8014c973d0817482237ff7243db
MrArmand <dellavmaster@gmail.com>
2023-05-06 20:09:00 +0100

    Options button

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,13 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-   public void PlayGame()
+    [SerializeField] GameObject mainMenuUI;
+    [SerializeField] GameObject optionsMenuUI;
+    public Animator transition;
+    public float transitionTime = 1f;
+
+    public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
     }
 
+    public void Settings()
+    {
+        mainMenuUI.SetActive(false);
+        optionsMenuUI.SetActive(true);
+    }
+
+    public void Back()
+    {
+        mainMenuUI.SetActive(true);
+        optionsMenuUI.SetActive(false);
+    }
     public void QuitGame()
     {
         Application.Quit();
     }
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(levelIndex);
+    }
 }
+
